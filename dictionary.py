7fa922de2efe90d06c61b78793c80501fa480516
Amin Moghtader <aminkant999@gmail.com>
2026-06-15 18:28:57 +0330

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,90 @@
+# ذخیره اطلاعات مربوط به یک پیتزا
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+}
+
+# نمایش خلاصه سفارش
+print(
+    f"شما یک پیتزای با خمیر {pizza['crust']} سفارش داده‌اید "
+    "با مخلفات زیر:"
+)
+
+for topping in pizza['toppings']:
+    print(f"\t{topping}")
+
+
+# دیکشنری تو در تو برای اطلاعات کاربران
+users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+    },
+
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+    },
+}
+
+# پیمایش کاربران و نمایش اطلاعات آن‌ها
+for username, user_info in users.items():
+
+    print(f"\nنام کاربری: {username}")
+
+    full_name = f"{user_info['first']} {user_info['last']}"
+    location = user_info['location']
+
+    print("نام:", full_name.title())
+    print("محل سکونت:", location.title())
+
+
+# زبان‌های برنامه‌نویسی مورد علاقه کاربران
+favorite_languages = {
+    'jen': ['python', 'rust'],
+    'sarah': ['c'],
+    'edward': ['rust', 'go'],
+    'phil': ['python', 'haskell'],
+}
+
+# نمایش زبان‌های مورد علاقه هر کاربر
+for name, languages in favorite_languages.items():
+
+    print(f"\nزبان‌های مورد علاقه {name.title()}:")
+
+    for language in languages:
+        print(f"\t{language.title()}")
+
+
+# ساخت یک لیست خالی برای نگهداری بیگانگان
+aliens = []
+
+# ایجاد 30 بیگانه سبزرنگ
+for alien_number in range(30):
+
+    new_alien = {
+        'color': 'green',
+        'points': 5,
+        'speed': 'slow'
+    }
+
+    aliens.append(new_alien)
+
+# تغییر مشخصات سه بیگانه اول
+for alien in aliens[:3]:
+
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+
+# نمایش پنج بیگانه اول
+for alien in aliens[:5]:
+    print(alien)
+
+print("...")
+
+# نمایش تعداد کل بیگانگان ساخته شده
+print(f"تعداد کل بیگانگان: {len(aliens)}")
